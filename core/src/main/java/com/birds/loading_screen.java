2606a5291897c4a6431108a407ff22bea6a99d0b
Atin <atin23157@iiitd.ac.in>
2024-10-22 00:44:37 +0530

    added assets for pause menu

--- core/src/main/java/com/birds/loading_screen.java
+++ core/src/main/java/com/birds/loading_screen.java
@@ -143,7 +143,18 @@ public class loading_screen implements Screen {
         assetManager.load("Credits.png", Texture.class);
         assetManager.load("Notification.png", Texture.class);
         assetManager.load("volume_bar.png", Texture.class);
-        assetManager.load("volume_bar_green.png", Texture.class);
+        assetManager.load("Pause_Menu.png", Texture.class);
+        assetManager.load("settings_button.png", Texture.class);
+        assetManager.load("play_button.png", Texture.class);
+        assetManager.load("rotate_button.png", Texture.class);
+        assetManager.load("save_button.png", Texture.class);
+
+
+
+
+
+
+
 
 
 
