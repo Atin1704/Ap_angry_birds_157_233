cc9fd5da3df9663bbbe93dfd3d869942182f91e3
Atin <atin23157@iiitd.ac.in>
2024-10-21 22:48:11 +0530

    ..

--- core/src/main/java/com/birds/saved_game_screen.java
+++ core/src/main/java/com/birds/saved_game_screen.java
@@ -57,9 +57,9 @@ public class saved_game_screen implements Screen {
 
         spriteBatch.draw(background_image, 0, 0, worldWidth, worldHeight);
         spriteBatch.draw(saved_top,23 , 64, 60, 20);
-        spriteBatch.draw(saved_1, 37,45 , 30, 13);
-        spriteBatch.draw(saved_2, 37, 30, 30, 13);
-        spriteBatch.draw(saved_3, 37, 15, 30, 13);
+        spriteBatch.draw(saved_1, 33,41 , worldWidth/3, worldHeight/10);
+        spriteBatch.draw(saved_2, 33,41 , worldWidth/3, worldHeight/10);
+        spriteBatch.draw(saved_3, 33,41 , worldWidth/3, worldHeight/10);
         spriteBatch.draw(exit_icon, 80, 3, worldWidth/8, worldHeight/12);
 
 
