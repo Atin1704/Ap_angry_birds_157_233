4dc821535cb60ce7130e124675a56d871fe9cc13
Atin <atin23157@iiitd.ac.in>
2024-11-25 22:16:51 +0530

    thoda

--- core/src/main/java/com/data/Glass_block.java
+++ core/src/main/java/com/data/Glass_block.java
@@ -1,38 +1,56 @@
+// Wood_block.java
 package com.data;
-import com.badlogic.gdx.assets.AssetManager;
+
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.birds.*;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
 import java.io.Serializable;
 
-public class Glass_block extends Block implements Serializable {
-    private Texture full_box;
-    private Texture stick_v;
-    private Texture stick_h;
-    private int health=1;
-    public Glass_block(AssetManager assetManager, SpriteBatch spriteBatch) {
-        super(spriteBatch, assetManager);
-        full_box = assetManager.get("Glass_block.png", Texture.class);
-        stick_v = assetManager.get("Glass_Stick_Vertical.png", Texture.class);
-        stick_h = assetManager.get("Glass_Stick_Horizontal.png", Texture.class);
-    }
-    @Override
-    public Texture getFull_box() {
-        return full_box;
-    }
-    @Override
-    public Texture getStick_v() {
-        return stick_v;
-    }
-    @Override
-    public Texture getStick_h() {
-        return stick_h;
-    }
-    static {
-        set_value();
-    }
-    //@Override
-    public static void set_value(){
+public class Glass_block extends Obstacle implements Serializable {
+    private Texture image;
+    private int health;
 
+    public Glass_block(World world, float xPos, float yPos, float width, float height) {
+        super(world, "Glass_block.png", xPos, yPos, width, height);
+        this.image = new Texture("Glass_block.png");
+        this.health = 10;
+        this.sprite = new Sprite(image);
+        this.sprite.setSize(width, height);
+        this.sprite.setPosition(xPos, yPos);
+        this.sprite.setOriginCenter();
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(xPos, yPos);
+        this.body = world.createBody(bodyDef);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(width / 2, height / 2);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1.0f;
+        fixtureDef.friction = 0.5f;
+        fixtureDef.restitution = 0.1f; // Low restitution coefficient
+        this.body.createFixture(fixtureDef);
+        shape.dispose();
+    }
+
+    public void update() {
+        Vector2 bodyPosition = body.getPosition();
+        sprite.setPosition(
+            bodyPosition.x - sprite.getWidth() / 2,
+            bodyPosition.y - sprite.getHeight() / 2
+        );
+    }
+
+    public void draw(SpriteBatch batch) {
+        sprite.draw(batch);
     }
 }
---
+++ core/src/main/java/com/data/Obstacle.java
@@ -0,0 +1,110 @@
+package com.data;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
+
+import java.io.Serializable;
+
+public class Obstacle implements Serializable {
+    protected float health;
+    protected float xPos;
+    protected float yPos;
+    protected float width;
+    protected float height;
+    protected Texture texture;
+    protected Sprite sprite;
+    protected Body body;
+    protected World world;
+    protected float radius;
+
+    public Obstacle(World world, String texturePath, float xPos, float yPos, float width, float height) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.width = width;
+        this.height = height;
+        this.health = 100;
+        texture = new Texture(texturePath);
+    }
+
+    public float getHealth() {
+        return health;
+    }
+
+    public void setHealth(float health) {
+        this.health = health;
+    }
+
+    public float getXPos() {
+        return xPos;
+    }
+
+    public void setXPos(float xPos) {
+        this.xPos = xPos;
+    }
+
+    public float getYPos() {
+        return yPos;
+    }
+
+    public void setYPos(float yPos) {
+        this.yPos = yPos;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
+
+    public void setTexture(Texture texture) {
+        this.texture = texture;
+    }
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = sprite;
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+    public World getWorld() {
+        return world;
+    }
+
+    public void setWorld(World world) {
+        this.world = world;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+}
