60a147019b9dbfde011c42fab3ae011646aef1c6
Atin <atin23157@iiitd.ac.in>
2024-11-26 03:22:40 +0530

    kuch to hogaya finally

--- core/src/main/java/com/birds/CollisionHandler.java
+++ core/src/main/java/com/birds/CollisionHandler.java
@@ -6,6 +6,19 @@ import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Manifold;
 
 public class CollisionHandler implements ContactListener {
+    //need to handle collisions between birds, pigs and obstacles
+    //Birds and pigs hsould collide and bird's damage should be subtracted from blocks,
+    //those blocks which have zero helath hsould stop renderinh
+    //similarly for pigs birds
+    //birds health hsould
+    //if the birds velocitybis less thna acertain velocity it hsould be removed as its damage baility ouwld be affcted
+    //if baird collides with the gound or nay of its fourd edge sit dies
+    //similar for any obstacle or pig
+    //pigs and blocks would only collide with eachother if the velocity difference between them is substantial
+    //if the velocity difference is less than a certain value they would not collide
+    //if the bird collides with the ground or any of its four edges it dies
+    //two birds wont collide with each other
+    //if one bird.body(is not awke) and it affects
 
 
 
