1430a1279354493f7532f32bb0e86c6dfedd01fa
Atin <atin23157@iiitd.ac.in>
2024-11-26 02:14:33 +0530

    kuch to hogaya finally

--- core/src/main/java/com/birds/level_1_screen.java
+++ core/src/main/java/com/birds/level_1_screen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
@@ -24,6 +25,8 @@ public class level_1_screen implements Screen {
     private final AssetManager assetManager;
     private Texture background_image;
     private Texture pause_button;
+    private Texture arrow_texture;
+    private Sprite arrow_sprite;
 
     private Main game_runner;
     private final SpriteBatch spriteBatch;
@@ -58,6 +61,9 @@ public class level_1_screen implements Screen {
         // Load textures
         background_image = new Texture("Level2_bg.png");
         pause_button = new Texture("Pause_icon.png");
+        arrow_texture = new Texture("arrow.png");
+        arrow_sprite = new Sprite(arrow_texture);
+        arrow_sprite.setOriginCenter();
 
         createGround();
         createBirds();
@@ -66,68 +72,67 @@ public class level_1_screen implements Screen {
         createPigs();
     }
 
-    // level_1_screen.java
-private void createGround() {
-    // Create ground
-    BodyDef groundBodyDef = new BodyDef();
-    groundBodyDef.type = BodyDef.BodyType.StaticBody;
-    groundBodyDef.position.set(new Vector2(600, 217));
-    Body groundBody = world.createBody(groundBodyDef);
+    private void createGround() {
+        // Create ground
+        BodyDef groundBodyDef = new BodyDef();
+        groundBodyDef.type = BodyDef.BodyType.StaticBody;
+        groundBodyDef.position.set(new Vector2(600, 217));
+        Body groundBody = world.createBody(groundBodyDef);
 
-    EdgeShape groundShape = new EdgeShape();
-    groundShape.set(new Vector2(-600, 0), new Vector2(600, 0));
-    FixtureDef groundFixtureDef = new FixtureDef();
-    groundFixtureDef.shape = groundShape;
-    groundFixtureDef.friction = 0.5f;
-    groundFixtureDef.restitution = 0.0f; // No bouncing
-    groundBody.createFixture(groundFixtureDef);
-    groundShape.dispose();
+        EdgeShape groundShape = new EdgeShape();
+        groundShape.set(new Vector2(-600, 0), new Vector2(600, 0));
+        FixtureDef groundFixtureDef = new FixtureDef();
+        groundFixtureDef.shape = groundShape;
+        groundFixtureDef.friction = 0.5f;
+        groundFixtureDef.restitution = 0.0f; // No bouncing
+        groundBody.createFixture(groundFixtureDef);
+        groundShape.dispose();
 
-    // Create left boundary
-    BodyDef leftBoundaryDef = new BodyDef();
-    leftBoundaryDef.type = BodyDef.BodyType.StaticBody;
-    leftBoundaryDef.position.set(new Vector2(0, 500));
-    Body leftBoundary = world.createBody(leftBoundaryDef);
+        // Create left boundary
+        BodyDef leftBoundaryDef = new BodyDef();
+        leftBoundaryDef.type = BodyDef.BodyType.StaticBody;
+        leftBoundaryDef.position.set(new Vector2(0, 500));
+        Body leftBoundary = world.createBody(leftBoundaryDef);
 
-    EdgeShape leftShape = new EdgeShape();
-    leftShape.set(new Vector2(0, -500), new Vector2(0, 500));
-    FixtureDef leftFixtureDef = new FixtureDef();
-    leftFixtureDef.shape = leftShape;
-    leftFixtureDef.friction = 0.5f;
-    leftFixtureDef.restitution = 0.0f; // No bouncing
-    leftBoundary.createFixture(leftFixtureDef);
-    leftShape.dispose();
+        EdgeShape leftShape = new EdgeShape();
+        leftShape.set(new Vector2(0, -500), new Vector2(0, 500));
+        FixtureDef leftFixtureDef = new FixtureDef();
+        leftFixtureDef.shape = leftShape;
+        leftFixtureDef.friction = 0.5f;
+        leftFixtureDef.restitution = 0.0f; // No bouncing
+        leftBoundary.createFixture(leftFixtureDef);
+        leftShape.dispose();
 
-    // Create right boundary
-    BodyDef rightBoundaryDef = new BodyDef();
-    rightBoundaryDef.type = BodyDef.BodyType.StaticBody;
-    rightBoundaryDef.position.set(new Vector2(1200, 500));
-    Body rightBoundary = world.createBody(rightBoundaryDef);
+        // Create right boundary
+        BodyDef rightBoundaryDef = new BodyDef();
+        rightBoundaryDef.type = BodyDef.BodyType.StaticBody;
+        rightBoundaryDef.position.set(new Vector2(1200, 500));
+        Body rightBoundary = world.createBody(rightBoundaryDef);
 
-    EdgeShape rightShape = new EdgeShape();
-    rightShape.set(new Vector2(0, -500), new Vector2(0, 500));
-    FixtureDef rightFixtureDef = new FixtureDef();
-    rightFixtureDef.shape = rightShape;
-    rightFixtureDef.friction = 0.5f;
-    rightFixtureDef.restitution = 0.0f; // No bouncing
-    rightBoundary.createFixture(rightFixtureDef);
-    rightShape.dispose();
+        EdgeShape rightShape = new EdgeShape();
+        rightShape.set(new Vector2(0, -500), new Vector2(0, 500));
+        FixtureDef rightFixtureDef = new FixtureDef();
+        rightFixtureDef.shape = rightShape;
+        rightFixtureDef.friction = 0.5f;
+        rightFixtureDef.restitution = 0.0f; // No bouncing
+        rightBoundary.createFixture(rightFixtureDef);
+        rightShape.dispose();
 
-    // Create top boundary
-    BodyDef topBoundaryDef = new BodyDef();
-    topBoundaryDef.type = BodyDef.BodyType.StaticBody;
-    topBoundaryDef.position.set(new Vector2(600, 1000));
-    Body topBoundary = world.createBody(topBoundaryDef);
+        // Create top boundary
+        BodyDef topBoundaryDef = new BodyDef();
+        topBoundaryDef.type = BodyDef.BodyType.StaticBody;
+        topBoundaryDef.position.set(new Vector2(600, 1000));
+        Body topBoundary = world.createBody(topBoundaryDef);
 
-    EdgeShape topShape = new EdgeShape();
-    topShape.set(new Vector2(-600, 0), new Vector2(600, 0));
-    FixtureDef topFixtureDef = new FixtureDef();
-    topFixtureDef.shape = topShape;
-    topFixtureDef.friction = 0.5f;
-    topFixtureDef.restitution = 0.0f; // No bouncing
-    topBoundary.createFixture(topFixtureDef);
-    topShape.dispose();
-}
+        EdgeShape topShape = new EdgeShape();
+        topShape.set(new Vector2(-600, 0), new Vector2(600, 0));
+        FixtureDef topFixtureDef = new FixtureDef();
+        topFixtureDef.shape = topShape;
+        topFixtureDef.friction = 0.5f;
+        topFixtureDef.restitution = 0.0f; // No bouncing
+        topBoundary.createFixture(topFixtureDef);
+        topShape.dispose();
+    }
 
     private void createBirds() {
         birds = new ArrayList<>();
@@ -193,83 +198,93 @@ private void createGround() {
             pig.draw(spriteBatch);
         }
         slingshot.draw(spriteBatch);
+
+        // Draw the arrow if dragging
+        if (isDragging) {
+            arrow_sprite.setPosition(slingshot.getX(), slingshot.getY() + slingshot.getHeight());
+            Vector2 direction = new Vector2(dragStart.x - touchPos.x, dragStart.y - touchPos.y);
+            arrow_sprite.setRotation(direction.angleDeg());
+            arrow_sprite.setSize(direction.len() / 2, arrow_sprite.getHeight());
+            arrow_sprite.draw(spriteBatch);
+        }
+
         spriteBatch.end();
 
         debugRenderer.render(world, viewport.getCamera().combined);
     }
 
     private void input() {
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
-            logger.info("ESCAPE key pressed, exiting");
-            Gdx.app.exit();
-        }
-        // Handle touch input for pause button
-        if (Gdx.input.justTouched()) {
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY());
-            viewport.unproject(touchPos);
+    if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+        logger.info("ESCAPE key pressed, exiting");
+        Gdx.app.exit();
+    }
+    // Handle touch input for pause button
+    if (Gdx.input.justTouched()) {
+        touchPos.set(Gdx.input.getX(), Gdx.input.getY());
+        viewport.unproject(touchPos);
 
-            float pauseButtonX = 1100f;
-            float pauseButtonY = 920f;
-            float pauseButtonWidth = 65f;
-            float pauseButtonHeight = 65f;
+        float pauseButtonX = 1100f;
+        float pauseButtonY = 920f;
+        float pauseButtonWidth = 65f;
+        float pauseButtonHeight = 65f;
 
-            if (touchPos.x >= pauseButtonX && touchPos.x <= (pauseButtonX + pauseButtonWidth)
-                    && touchPos.y >= pauseButtonY && touchPos.y <= (pauseButtonY + pauseButtonHeight)) {
-                logger.info("Pause button clicked");
-                game_runner.click.play();
-                Timer.schedule(new Timer.Task() {
-                    @Override
-                    public void run() {
-                        game_runner.setScreen(new pause_screen(game_runner, assetManager, 1));
-                    }
-                }, 0.25f);  // Delay of 0.25 seconds
-            }
-        }
-
-        // Handle "Enter" key press for victory screen
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
-            logger.info("ENTER key pressed, switching to Victory_Screen");
-            Victory_Screen v1 = new Victory_Screen(game_runner, assetManager, 1);
-            game_runner.setScreen(v1);
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.DEL)) {
-            logger.info("DEL key pressed, switching to Defeat_Screen");
-            game_runner.setScreen(new Defeat_Screen(game_runner, assetManager, 1));
-        }
-
-        // Handle bird selection
-        if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_1) && birds.size() > 0) {
-            currentBird = birds.get(0);
-            placeBirdOnSlingshot(currentBird);
-        } else if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_2) && birds.size() > 1) {
-            currentBird = birds.get(1);
-            placeBirdOnSlingshot(currentBird);
-        } else if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_3) && birds.size() > 2) {
-            currentBird = birds.get(2);
-            placeBirdOnSlingshot(currentBird);
-        }
-
-        // Handle dragging input for slingshot
-        if (Gdx.input.isTouched() && currentBird != null && !currentBird.isLaunched()) {
-            if (!isDragging) {
-                isDragging = true;
-                dragStart.set(Gdx.input.getX(), Gdx.input.getY());
-                viewport.unproject(dragStart);
-            }
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY());
-            viewport.unproject(touchPos);
-            slingshot.drag(touchPos.x, touchPos.y);
-        } else if (isDragging) {
-            isDragging = false;
-            Vector2 dragEnd = new Vector2(Gdx.input.getX(), Gdx.input.getY());
-            viewport.unproject(dragEnd);
-            Vector2 velocity = new Vector2(dragStart.x - dragEnd.x, dragStart.y - dragEnd.y).scl(5);
-            currentBird.setVelocity(velocity);
-            currentBird.setAwake(true);
-            currentBird.setLaunched(true);
-            currentBird = null; // Allow selecting a new bird
+        if (touchPos.x >= pauseButtonX && touchPos.x <= (pauseButtonX + pauseButtonWidth)
+                && touchPos.y >= pauseButtonY && touchPos.y <= (pauseButtonY + pauseButtonHeight)) {
+            logger.info("Pause button clicked");
+            game_runner.click.play();
+            Timer.schedule(new Timer.Task() {
+                @Override
+                public void run() {
+                    game_runner.setScreen(new pause_screen(game_runner, assetManager, 1));
+                }
+            }, 0.25f);  // Delay of 0.25 seconds
         }
     }
+
+    // Handle "Enter" key press for victory screen
+    if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
+        logger.info("ENTER key pressed, switching to Victory_Screen");
+        Victory_Screen v1 = new Victory_Screen(game_runner, assetManager, 1);
+        game_runner.setScreen(v1);
+    }
+    if (Gdx.input.isKeyJustPressed(Input.Keys.DEL)) {
+        logger.info("DEL key pressed, switching to Defeat_Screen");
+        game_runner.setScreen(new Defeat_Screen(game_runner, assetManager, 1));
+    }
+
+    // Handle bird selection
+    if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_1) && birds.size() > 0 && !birds.get(0).isLaunched()) {
+        currentBird = birds.get(0);
+        placeBirdOnSlingshot(currentBird);
+    } else if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_2) && birds.size() > 1 && !birds.get(1).isLaunched()) {
+        currentBird = birds.get(1);
+        placeBirdOnSlingshot(currentBird);
+    } else if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_3) && birds.size() > 2 && !birds.get(2).isLaunched()) {
+        currentBird = birds.get(2);
+        placeBirdOnSlingshot(currentBird);
+    }
+
+    // Handle dragging input for slingshot
+    if (Gdx.input.isTouched() && currentBird != null && !currentBird.isLaunched()) {
+        if (!isDragging) {
+            isDragging = true;
+            dragStart.set(Gdx.input.getX(), Gdx.input.getY());
+            viewport.unproject(dragStart);
+        }
+        touchPos.set(Gdx.input.getX(), Gdx.input.getY());
+        viewport.unproject(touchPos);
+        slingshot.drag(touchPos.x, touchPos.y);
+    } else if (isDragging) {
+        isDragging = false;
+        Vector2 dragEnd = new Vector2(Gdx.input.getX(), Gdx.input.getY());
+        viewport.unproject(dragEnd);
+        Vector2 velocity = new Vector2(dragStart.x - dragEnd.x, dragStart.y - dragEnd.y).scl(5);
+        currentBird.setVelocity(velocity);
+        currentBird.setAwake(true);
+        currentBird.setLaunched(true);
+        currentBird = null; // Allow selecting a new bird
+    }
+}
 
     @Override
     public void resize(int width, int height) {
@@ -300,5 +315,6 @@ private void createGround() {
         shapeRenderer.dispose();
         background_image.dispose();
         pause_button.dispose();
+        arrow_texture.dispose();
     }
 }
