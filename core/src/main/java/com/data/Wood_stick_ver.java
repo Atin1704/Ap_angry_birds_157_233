42ecb1f21ce664f506ec5c5911b0736c5ce4520d
Atin <atin23157@iiitd.ac.in>
2024-11-26 10:59:22 +0530

    sticks added

---
+++ core/src/main/java/com/data/Wood_stick_ver.java
@@ -0,0 +1,40 @@
+package com.data;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Wood_stick_ver extends Obstacle{
+    private Texture image;
+    private int health;
+
+    public Wood_stick_ver(World world, float xPos, float yPos, float width, float height) {
+        super(world, "Wooden_Stick_Vertical.png", xPos, yPos, width, height);
+        this.image = new Texture("Wooden_Stick_Vertical.png");
+        this.health = 10;
+        this.sprite = new Sprite(image);
+        this.sprite.setSize(width, height);
+        this.sprite.setPosition(xPos, yPos);
+        this.sprite.setOriginCenter();
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(xPos, yPos);
+        this.body = world.createBody(bodyDef);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(width / 2, height / 2);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1.0f;
+        fixtureDef.friction = 0.9f;
+        fixtureDef.restitution = 0.0f; // Low restitution coefficient
+        this.body.createFixture(fixtureDef);
+        shape.dispose();
+        this.body.setAwake(false);
+    }
+}
